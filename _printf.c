44215956bd249a97cedbe8f75fe3f01273a7b22e
Benard <githubenard16@gmail.com>
2023-04-25 16:52:17 +0300

    fix

--- _printf.c
+++ _printf.c
@@ -1,53 +1,51 @@
-#include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
 
 /**
- * _printf - prints output according to a format
- * @format: character string containing zero or more directives
- *
- * Return: number of characters printed (excluding the null byte used to end output to strings)
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print
+ * Return: characters printed.
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
+	va_list args;
+	int printed_chars = 0;
 
-    va_start(args, format);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format++ == '%')
+		{
+			switch (*format++)
+			{
+				case 'c':
+				{
+					char c = va_arg(args, int);
 
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;  /* move past the '%' character */
-            switch (*format)
-            {
-                case 'c':
-                    count += write(1, va_arg(args, int *), 1);
-                    break;
+					printed_chars += write(1, &c, 1);
+					break;
+				}
+				case 's':
+				{
+					char *str = va_arg(args, char *);
 
-                case 's':
-                    count += write(1, va_arg(args, char *), strlen(va_arg(args, char *)));
-                    break;
-
-                case '%':
-                    count += write(1, "%", 1);
-                    break;
-
-                default:
-                    /* unsupported conversion specifier, do nothing */
-                    break;
-            }
-        }
-        else
-        {
-            count += write(1, format, 1);
-        }
-        format++;
-    }
-
-    va_end(args);
-
-    return count;
+					if (!str)
+						str = "(null)";
+					while (*str)
+						printed_chars += write(1, str++, 1);
+					break;
+				}
+				case '%':
+					printed_chars += write(1, "%", 1);
+					break;
+				default:
+					printed_chars += write(1, &format[-2], 2);
+					break;
+			}
+		}
+		else
+			printed_chars += write(1, format - 1, 1);
+	}
+	va_end(args);
+	return (printed_chars);
 }
-
--- main.h
+++ main.h
@@ -3,8 +3,21 @@
 
 #include <stdarg.h>
 #include <unistd.h>
-#include <string.h>
 
+/**
+ * struct conversion_specifier - contains specifier and function
+ * @format: specifier character
+ * @func: a pointer to function that handles the conversion specifier
+ */
+typedef struct conversion_specifier
+{
+	char format;
+	int (*func)(va_list);
+} conv_spec;
+
+int print_char(va_list args);
+int print_str(va_list args);
+int print_percent(va_list args);
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
