c577a606b110281bbad131fdcb8aa0d45f30398a
Benard <githubenard16@gmail.com>
2023-04-25 12:30:29 +0300

    fix main.h and _printf.c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,57 @@
-#include <stdarg.h>
-#include <unistd.h>
+#include "main.h"
+
+/**
+ * struct conversion_specifier - a struct that contains a conversion specifier and its corresponding function
+ * @format: the conversion specifier character
+ * @func: a pointer to the function that handles the conversion specifier
+ */
+typedef struct conversion_specifier
+{
+    char format;
+    int (*func)(va_list);
+} conv_spec;
+
+/**
+ * print_char - prints a single character to stdout
+ * @args: a va_list containing the character to print
+ *
+ * Return: the number of characters printed
+ */
+int print_char(va_list args)
+{
+    char c = va_arg(args, int);
+    return (write(1, &c, 1));
+}
+
+/**
+ * print_str - prints a string to stdout
+ * @args: a va_list containing the string to print
+ *
+ * Return: the number of characters printed
+ */
+int print_str(va_list args)
+{
+	int printed_chars = 0;
+
+    char *str = va_arg(args, char *);
+    if (!str)
+        str = "(null)";
+    while (*str)
+        printed_chars += write(1, str++, 1);
+    return (printed_chars);
+}
+
+/**
+ * print_percent - prints a single percent character to stdout
+ * @args: a va_list (unused)
+ *
+ * Return: the number of characters printed
+ */
+int print_percent(va_list args)
+{
+    (void) args;
+    return (write(1, "%", 1));
+}
 
 /**
  * _printf - Outputs a formatted string.
@@ -9,45 +61,40 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int printed_chars = 0;
+	conv_spec specs[] = {
+        {'c', print_char},
+        {'s', print_str},
+        {'%', print_percent},
+        {'\0', NULL}
+    };
+    
+    va_list args;
+    int printed_chars = 0;
 
-	va_start(args, format);
-	while (*format)
-	{
-		if (*format++ == '%')
-		{
-			switch (*format++)
-			{
-				case 'c':
-				{
-					char c = va_arg(args, int);
+    va_start(args, format);
 
-					printed_chars += write(1, &c, 1);
-					break;
-				}
-				case 's':
-				{
-					char *str = va_arg(args, char *);
+    while (*format)
+    {
+        if (*format++ == '%')
+        {
+            int i = 0;
+            while (specs[i].format != '\0' && specs[i].format != *format)
+                i++;
 
-					if (!str)
-						str = "(null)";
-					while (*str)
-						printed_chars += write(1, str++, 1);
-					break;
-				}
-				case '%':
-					printed_chars += write(1, "%", 1);
-					break;
-				default:
-					printed_chars += write(1, &format[-2], 2);
-					break;
-			}
-		}
-		else
-			printed_chars += write(1, format - 1, 1);
-	}
-	va_end(args);
-	return (printed_chars);
+            if (specs[i].func != NULL)
+                printed_chars += specs[i].func(args);
+            else
+            {
+                printed_chars += write(1, &format[-2], 2);
+                if (*format != '\0')
+                    printed_chars += write(1, format, 1);
+            }
+        }
+        else
+            printed_chars += write(1, format - 1, 1);
+    }
+
+    va_end(args);
+    return (printed_chars);
 }
 
--- main.h
+++ main.h
@@ -4,6 +4,20 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/**
+ * struct conversion_specifier - a struct that contains a conversion specifier and its corresponding function
+ * @format: the conversion specifier character
+ * @func: a pointer to the function that handles the conversion specifier
+ */
+typedef struct conversion_specifier
+{
+    char format;
+    int (*func)(va_list);
+} conv_spec;
+
+int print_char(va_list args);
+int print_str(va_list args);
+int print_percent(va_list args);
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
