0a8703755dcaa70847579f063ca7763b4e4f2127
Benard <githubenard16@gmail.com>
2023-04-25 11:59:21 +0300

    add main.h and _printf.c

--- _printf.c
+++ _printf.c
@@ -1,66 +1,51 @@
 #include <stdarg.h>
 #include <unistd.h>
-
 /**
- * _printf - produces output according to a format.
- * @format: format string
- * Return: the number of characters printed
- */
+* _printf - prints according to a format.
+* @format: format string
+* Return: no of characters printed
+*/
 int _printf(const char *format, ...)
-{
-	int printed_chars = 0;
+{int printed_chars = 0;
 	va_list list;
-
 	va_start(list, format);
 
-	while (*format)
+	for (; *format; format++)
 	{
 		if (*format == '%')
 		{
 			format++;
-
 			if (*format == 'c')
 			{
 				char c = va_arg(list, int);
 
-				write(1, &c, 1);
-				printed_chars++;
+				printed_chars += write(1, &c, 1);
 			}
 			else if (*format == 's')
 			{
 				char *str = va_arg(list, char *);
-
 				if (str == NULL)
 					str = "(null)";
-				while (*str)
+				for (; *str; str++)
 				{
-					write(1, str, 1);
-					str++;
-					printed_chars++;
+					printed_chars += write(1, str, 1);
 				}
 			}
 			else if (*format == '%')
 			{
-				write(1, "%", 1);
-				printed_chars++;
+				printed_chars += write(1, "%", 1);
 			}
 			else
 			{
-				write(1, &format[-1], 1);
-				write(1, &format[0], 1);
-				printed_chars += 2;
+				printed_chars += write(1, &format[-1], 1);
+				printed_chars += write(1, &format[0], 1);
 			}
 		}
 		else
 		{
-			write(1, format, 1);
-			printed_chars++;
+			printed_chars += write(1, format, 1);
 		}
-
-		format++;
 	}
-
 	va_end(list);
-
 	return (printed_chars);
 }
