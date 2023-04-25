54fc7d54dd1d8817f8d551eee61aa25721d5de2e
Benard <githubenard16@gmail.com>
2023-04-25 16:27:54 +0300

    fix

--- _printf.c
+++ _printf.c
@@ -1,51 +1,91 @@
-#include <stdarg.h>
-#include <unistd.h>
+#include "main.h"
+
+void clear_printf_args(va_list args, buffer_t *output);
+int execute_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
+
+/**
+ * clear_printf_args - Cleans up arguments passed to _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
+ */
+void clear_printf_args(va_list args, buffer_t *output)
+{
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
+
+/**
+ * execute_printf - Processes the format string for _printf.
+ * @format: Character string to print - may contain directives.
+ * @output: A buffer_t struct containing a buffer.
+ * @args: A va_list of arguments.
+ *
+ * Return: The number of characters stored to output.
+ */
+int execute_printf(const char *format, va_list args, buffer_t *output)
+{
+	int i, width, precision, result = 0;
+	char temp;
+	unsigned char flags, length;
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
+
+	for (i = 0; *(format + i); i++)
+	{
+		length = 0;
+		if (*(format + i) == '%')
+		{
+			temp = 0;
+			flags = handle_flags(format + i + 1, &temp);
+			width = handle_width(args, format + i + temp + 1, &temp);
+			precision = handle_precision(args, format + i + temp + 1,
+					&temp);
+			length = handle_length(format + i + temp + 1, &temp);
+
+			f = handle_specifiers(format + i + temp + 1);
+			if (f != NULL)
+			{
+				i += temp + 1;
+				result += f(args, output, flags, width, precision, length);
+				continue;
+			}
+			else if (*(format + i + temp + 1) == '\0')
+			{
+				result = -1;
+				break;
+			}
+		}
+		result += _memcpy(output, (format + i), 1);
+		i += (length != 0) ? 1 : 0;
+	}
+	clear_printf_args(args, output);
+	return (result);
+}
 
 /**
  * _printf - Outputs a formatted string.
- * @format: Character string to print
- * Return: characters printed.
+ * @format: Character string to print - may contain directives.
+ *
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
+	buffer_t *output;
 	va_list args;
-	int printed_chars = 0;
+	int result;
+
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
 
 	va_start(args, format);
-	while (*format)
-	{
-		if (*format++ == '%')
-		{
-			switch (*format++)
-			{
-				case 'c':
-				{
-					char c = va_arg(args, int);
 
-					printed_chars += write(1, &c, 1);
-					break;
-				}
-				case 's':
-				{
-					char *str = va_arg(args, char *);
+	result = execute_printf(format, args, output);
 
-					if (!str)
-						str = "(null)";
-					while (*str)
-						printed_chars += write(1, str++, 1);
-					break;
-				}
-				case '%':
-					printed_chars += write(1, "%", 1);
-					break;
-				default:
-					printed_chars += write(1, &format[-2], 2);
-					break;
-			}
-		}
-		else
-			printed_chars += write(1, format - 1, 1);
-	}
-	va_end(args);
-	return (printed_chars);
+	return (result);
 }
+
--- main.h
+++ main.h
@@ -2,23 +2,34 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 /**
- * struct conversion_specifier - contains specifier and function
- * @format: specifier character
- * @func: a pointer to function that handles the conversion specifier
- */
-typedef struct conversion_specifier
-{
-	char format;
-	int (*func)(va_list);
-} conv_spec;
 
-int print_char(va_list args);
-int print_str(va_list args);
-int print_percent(va_list args);
+struct buffer - buffer structure
+@start: pointer to the start of the buffer
+@len: length of the buffer
+@size: size of the buffer
+*/
+typedef struct buffer
+{
+char *start;
+int len;
+int size;
+} buffer_t;
+/* printf functions */
+void clear_printf_args(va_list args, buffer_t *output);
+int execute_printf(const char *format, va_list args, buffer_t *output);
 int _printf(const char *format, ...);
+void free_buffer(buffer_t *output);
+unsigned char handle_flags(const char *format, char *temp);
+int handle_width(va_list args, const char *format, char *temp);
+int handle_precision(va_list args, const char *format, char *temp);
+unsigned char handle_length(const char *format, char *temp);
+unsigned int (*handle_specifiers(const char *format))(va_list, buffer_t *,
+unsigned char, int, int, unsigned char);
+int _memcpy(buffer_t *output, const char *src, int n);
+buffer_t *init_buffer(void);
 
 #endif /* MAIN_H */
-
